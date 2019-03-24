4b841c0523e6447115cc9d282d530f312c34f6e6
LeeAnna Misterek <leeanna.misterek@gmail.com>
2019-03-24 16:24:57 -0700

    index page is now showing.

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-// var url = require('url');
-// var fs = require('fs');
+var url = require('url');
+var fs = require('fs');
 
-// function renderHTML (path, response) {
+function renderHTML (path, response) {
 	
-// 	fs.readFile(path, null, function(error, data) {
-// 		if(error) {
-// 			response.writeHead(404);
-// 		}
+	fs.readFile(path, null, function(error, data) {
+		if(error) {
+			response.writeHead(404);
+		}
 
-// 		else {
-// 			response.write(data);
-// 		}
+		else {
+			response.write(data);
+		}
 
-// 		response.end();
-// 	});
-// }
+		response.end();
+	});
+}
 
 
 module.exports = {
@@ -26,7 +26,7 @@ module.exports = {
 
 		switch (path) {
 			case '/': 
-				renderHTML('./index.html', response);
+				renderHTML('./public/index.html', response);
 				break;
 			case '/login':
 				renderHTML('./login.html', response);
@@ -42,37 +42,34 @@ module.exports = {
 
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+// var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
 var app = express();
 
-// view engine setup
-// app.set('view', path.join(_dirname, 'views'));
-app.use('/view', express.static("views"));
-app.set('view engine', 'jade');
+
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(_dirname, 'public', 'favicon.ico')));
-// app.use(express.static("public" ))
+// app.use(favicon(path.join(_dirname, 'public', 'favicon.ico')));
+
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(cookieParser());
-app.use(express.static(path.join(_dirname, 'public')));
+app.use(express.static("public"));
+app.use(express.static("public/favicon.ico"));
 
 app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to event handler
 app.use(function (req, res, next) {
+	console.log("here");
 	var err = new Error ('Not Found');
 	err.status = 404;
 	next(err);
